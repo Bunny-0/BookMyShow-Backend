1d8801434c77a03289f7a7730d3e019b40f18b81
shubham gaurav <shubhamgaurav2@gmail.com>
2023-09-28 20:31:06 +0530

    Remaining Files

---
+++ src/main/java/com/example/Project1/BookMyShowBackend/Enum/SeatType.java
@@ -0,0 +1,6 @@
+package com.example.Project1.BookMyShowBackend.Enum;
+
+public enum SeatType {
+    CLASSIC,
+    PREMIUM;
+}
---
+++ src/test/java/com/example/Project1/BookMyShowBackend/BookMyShowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project1.BookMyShowBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
